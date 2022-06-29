1deffeacb2223a94ebc86dd89e4425a2b1e463a6
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-29 10:40:48 +0500

    Create code3.py

---
+++ 6.) Relay Module/code3.py
@@ -0,0 +1,12 @@
+from machine import Pin
+from time import sleep
+
+# ESP32 GPIO 26
+relay = Pin(26, Pin.OUT)
+
+# ESP8266 GPIO 5
+#relay = Pin(5, Pin.OUT)
+
+while True:
+  relay.value(not relay.value())
+  sleep(4)
