7741c17b7fd6d345198d30c960488b75b4a46118
Muhammad Raheel <63813881+MuhammadRaheelNaseem@users.noreply.github.com>
2022-06-29 10:25:08 +0500

    Create pir5.py

---
+++ 4.) PIR Sensor/pir5.py
@@ -0,0 +1,29 @@
+from machine import Pin
+from time import time
+
+start_timer = False
+motion = False
+last_motion_time = 0
+delay_interval = 20
+
+def handle_interrupt(pin):
+  global motion, last_motion_time, start_timer
+  motion = True
+  start_timer = True
+  last_motion_time = time()
+
+led = Pin(12, Pin.OUT)
+pir = Pin(15, Pin.IN)
+
+pir.irq(trigger=Pin.IRQ_RISING, handler=handle_interrupt)
+
+while True:
+  if motion and start_timer:
+    print('Motion detected!')
+    led.value(1)
+    start_timer = False
+
+  elif motion and (time() - last_motion_time)>delay_interval:
+    print('Motion stopped!')
+    led.value(0)
+    motion = False
